94fc47bf10a3056370a022bc8967e79d79d99664
Doug Wright <dw@nof49.ca>
2021-01-13 20:27:31 -0500

    Completed exercise 3

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -3,17 +3,25 @@ import styled from "styled-components";
 
 import Header from "./Header";
 import ActionBar from "./ActionBar";
+import Stats from './Stats';
 import { TweetContext } from '../TweetContext';
 
 const Tweet = () => {
   // Use content
-  const { tweetContents, now } = useContext(TweetContext);
+  const {
+    tweetContents,
+    now,
+    numOfLikes,
+    numOfRetweets
+  } = useContext(TweetContext);
 
   return (
     <Wrapper>
       <Header />
       <TweetContents>{tweetContents}</TweetContents>
       <Timestamp>{now}</Timestamp>
+      <Divider />
+      <Stats numOfLikes={numOfLikes} numOfRetweets={numOfRetweets} />
       <Divider />
       <ActionBar />
       <Divider />
@@ -44,12 +52,6 @@ const Timestamp = styled.div`
 const Divider = styled.div`
   height: 1px;
   background: rgb(230, 236, 240);
-`;
-
-const Stats = styled.div`
-  display: flex;
-  align-items: center;
-  height: 48px;
 `;
 
 export default Tweet;
--- src/components/TweetContext.js
+++ src/components/TweetContext.js
@@ -1,27 +1,32 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import moment from 'moment';
 import avatar from "../assets/carmen-sandiego.png";
 
 export const TweetContext = createContext(null);
 
 export const TweetProvider = ({ children }) => {
+  const [numOfLikes, setNumOfLikes] = useState(460);
+  const [numOfRetweets, setNumOfRetweets] = useState(65);
+  const [isLiked, setIsLiked] = useState(false);
+  const [isRetweeted, setIsRetweeted] = useState(false);
+
   const tweetContents = "Where in the world am I?";
   const displayName = "Carmen Sandiego ✨";
   const username = "carmen-sandiego";
   const avatarSrc = avatar;
-  const isRetweetedByCurrentUser = false;
-  const isLikedByCurrentUser = false;
-  const now = moment().format('h:m A - MMM DDDo, YYYY');
+  const now = moment().format('h:mm A - MMM DDDo, YYYY');
 
   return (
     <TweetContext.Provider
       value={{
+        numOfLikes,
+        numOfRetweets,
+        isRetweeted,
+        isLiked,
         tweetContents,
         displayName,
         username,
         avatarSrc,
-        isRetweetedByCurrentUser,
-        isLikedByCurrentUser,
         now
       }}
     >
