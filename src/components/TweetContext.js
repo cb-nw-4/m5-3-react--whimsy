13eb6b2fd70e35a31c7a3bfd77238c07a834437e
chris shino <chris.shino@hotmail.com>
2021-01-15 18:35:48 -0500

    done # 4

--- src/components/TweetContext.js
+++ src/components/TweetContext.js
@@ -16,10 +16,17 @@ export const TweetProvider = ({children}) => {
   let isRetweetedByCurrentUser=isRetweeted
   let isLikedByCurrentUser=isLiked
   const date = moment().format('h:m a - MMM Do, YYYY')
-
+  const handleToggleLike = () => {
+      setIsLiked(!isLiked)
+      isLiked == false ? setNumLikes(numLikes + 1) : setNumLikes(numLikes - 1)  
+  }
+  const handleToggleRetweet = () => {
+    setIsRetweeted(!isRetweeted)
+    isRetweeted == false ? setNumOfRetweets(numOfRetweets + 1) : setNumOfRetweets(numOfRetweets - 1)
+  }
   return (
     <TweetContext.Provider value={{
-      tweetContents, displayName, username, avatarSrc, isRetweetedByCurrentUser, isLikedByCurrentUser, date, numLikes, numOfRetweets
+      tweetContents, displayName, username, avatarSrc, isRetweetedByCurrentUser, isLikedByCurrentUser, date, numLikes, numOfRetweets, handleToggleLike, handleToggleRetweet
     }}>
       {children}
     </TweetContext.Provider>
