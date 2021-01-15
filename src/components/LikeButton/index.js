610735f8071d3f3b66fd22d6478d543d075b8f13
Sandra <sandrabess@hotmail.com>
2021-01-15 17:04:17 -0500

    exercice 6

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -4,6 +4,7 @@ import { TweetContext } from '../TweetContext';
 
 import Heart from "./Heart";
 import PoppingCircle from "./PoppingCircle";
+import ScaleIn from "./ScaleIn";
 
 const PARTICLE_COLORS = ["#e53935", "#1e88e5", "#43a047", "#fdd835", "#fb8c00"];
 
@@ -14,7 +15,12 @@ const LikeButton = ({ size = 40 }) => {
   return (
     <Wrapper style={{ width: size, height: size }}>
       {isLiked && <PoppingCircle size={`${size}px`} color="#E790F7" />}
-      <Heart width={heartSize} isToggled={isLiked} />
+      {isLiked ? (
+        <ScaleIn>
+          <Heart width={heartSize} isToggled={isLiked} />
+        </ScaleIn>
+          ) : (
+            <Heart width={heartSize} isToggled={isLiked} />)}        
     </Wrapper>
   );
 };
