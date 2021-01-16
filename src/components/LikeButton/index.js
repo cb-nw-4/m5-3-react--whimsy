7efac04d72da511b0f7127402589bc70704a9610
“Winnie <chanwinnie.wc@gmail.com>
2021-01-16 12:00:34 -0500

    Exercise 6

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Heart from "./Heart";
 import { TweetContext } from "../TweetContext";
 import PoppingCircle from "./PoppingCircle";
+import ScaleIn from "./ScaleIn";
  
 const PARTICLE_COLORS = ["#e53935", "#1e88e5", "#43a047", "#fdd835", "#fb8c00"];
 
@@ -12,10 +13,16 @@ const LikeButton = ({ size = 40 }) => {
   const { isLikedByCurrentUser } = useContext(TweetContext);
 
   return (
-    <Wrapper style={{ width: size, height: size }}>
-      <Heart width={heartSize} isToggled={isLikedByCurrentUser} />
+    <Wrapper style={{ width: size, height: size }}>  
+      {isLikedByCurrentUser ? (
+        <ScaleIn>
+          <Heart width={heartSize} isToggled={isLikedByCurrentUser} />
+        </ScaleIn>
+      ) : (
+        <Heart width={heartSize} isToggled={isLikedByCurrentUser} />
+      )}
       {isLikedByCurrentUser && <PoppingCircle size={size} color="#E790F7" />}
-    </Wrapper>
+  </Wrapper>  
   );
 };
 
